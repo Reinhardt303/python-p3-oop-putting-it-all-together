eeb5458a512eb0b9b8cbe3aee8817dbaa5edaec1
Thomas <thomasrains01@gmail.com>
2025-01-20 17:57:34 -0800

    odd that the putting it all together was easier than the labs

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+
+    def __init__(self, title = 'who reads books', page_count = 0):
+        self.page_count = page_count
+        self.title = title
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+        
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    def turn_page(self):
+        self.page_count += 1
+        print("Flipping the page...wow, you read fast!")
     
         
